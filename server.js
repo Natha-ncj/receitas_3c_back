4c57c6631436c5d7f5655b788873f85a563b1f6d
NATHAN CAMPOS JUSTEN <nathan_justen@estudante.sesisenai.org.br>
2026-03-02 11:50:47 -0300

    pa

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 import Fastify from 'fastify'
+import { Pool } from 'pg'
 
 const server = Fastify()
 
-server.get('/usuario', () => {
-    return 'pego fogo :)'
+const sql = new Pool({
+    user: "postgres",
+    password: "senai",
+    host: "localhost",
+    port: 5432,
+    database: "receitas"
+})
+
+server.get('/usuario', async () => {
+    const resultado = await sql.query('select * from usuario')
+    return resultado.rows
+})
+
+server.post('/usuario', async (request, reply) => {
+    const nome = request.body.nome
+    const senha = request.body.senha
+    const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)', [nome, senha])
+    return 'Usuario cadastrado!'
 })
 
 server.listen({
