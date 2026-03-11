e3943a710853d855acb64c1c076750f3964b554e
NATHAN CAMPOS JUSTEN <nathan_justen@estudante.sesisenai.org.br>
2026-03-11 09:02:05 -0300

    atualizado 1.2.1

--- server.js
+++ server.js
@@ -19,13 +19,33 @@ server.get('/usuario', async () => {
 server.post('/usuario', async (request, reply) => {
     const nome = request.body.nome
     const senha = request.body.senha
+
+    if (!nome || !senha){
+        reply.status.apply(400).send({error: 'Nome ou senha invalidos'});
+        }
+    
+
     const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)', [nome, senha])
-    return 'Usuario cadastrado!'
+    reply.status(201).send({mensagem: "Deu Certo!"})
+
 })
 
 server.put('/usuario/:id', async (request, reply) => {
     const body = request.body
     const id = request.params.id
+
+    if (!body || !body.nome || !body.senha) {
+        reply.status(400).send({error: 'nome ou senha invalido!'})
+    } else if(!id) {
+        reply.status(400).send({error: 'Faltou o ID!'})
+    }
+
+    const existe = await sql.query('select * from usuario where id = $1', [id])
+    
+    if (existe.rows.length === 0) {
+        reply.status(400).send({error: 'Usuário não existe no banco de dados'})
+    }
+
     const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2 WHERE  id = $3', [body.nome, body.senha, id])
     return 'Usuario alterado!'
 })
